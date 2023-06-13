c42b257ec6e730767c09d7dcbe9b08119f005711
Udayon-Paul-Dhrubo <1805109@ugrad.cse.buet.ac.bd>
2023-06-13 16:18:48 +0600

    good setup practise

---
+++ output/index.js
@@ -0,0 +1,4 @@
+"use strict";
+//console.log('Hello World');
+const str = 'Hello World';
+console.log(str);
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+//console.log('Hello World');
+
+const str = 'Hello World';
+console.log(str);
