f2553905384c715646582f134f0adc4f90874c97
Udayon-Paul-Dhrubo <1805109@ugrad.cse.buet.ac.bd>
2023-06-14 23:45:03 +0600

    dynamic type

---
+++ src/dynamic_any_type.ts
@@ -0,0 +1,19 @@
+let any_type; // by default any type
+
+// or explicitly we can declare
+let any_type_2 : any;
+
+
+// --------------------- Array ------------------ //
+let any_type_arr : any[] = [];
+
+any_type_arr.push(1);
+any_type_arr.push("string");
+
+
+
+// --------------------- Object ------------------ //
+let any_type_obj :{
+    name : any,
+    age : any
+};
